9745a3a556a163c1de2c01077ba4628db816e806
Dave Ray <daveray@gmail.com>
2011-08-12 00:14:18 -0400

    Wrote readme usage up describing everything and then did a quick, messy implementation of it. Seems to kinda work.

--- README.md
+++ README.md
@@ -1,9 +1,114 @@
 # dorothy
+Dorothy isn't usable yet. Soon.
 
 [Hiccup-style] (https://github.com/weavejester/hiccup) generation of [Graphviz] (http://www.graphviz.org/) graphs in Clojure.
 
 ## Usage
 
+A graph consists of a vector of *statements*.
+
+### Node Statement
+A *node statement* defines a node in the graph. It can take two forms:
+
+    node-id
+
+    [node-id]
+    
+    [node-id { attr map }]
+
+where `node-id` is a string or keyword with optional trailing *port* and *compass-point*. Here are some node statement examples:
+
+    :node0          ; Define a node called "node0"
+
+    :node0:port0    ; Define a node called "node0" with port "port0"
+
+    :node0:port0:sw ; Similarly a node with southwest compass point 
+
+the node's attr map is a map of attributes for the node. For example,
+
+    [:start {:shape :Mdiamond}]
+    ; => start [shape=Mdiamond];
+
+### Edge Statement
+An *edge statement* defines an edge in the graph. It is expressed as a vector with two or more node-ids followed optional attribute map:
+
+    [node-id0 node-id1 ... node-idN { attr map }]
+    ; => "node-id0" -> "node-id1" -> ... -> "node-idN" [attrs ...];
+
+In addition to node ids, an edge statement may also contain subgraphs:
+
+    [:start (subgraph [... subgraph statements ...])]
+
+For readability, `:>` delimiters may be optionally included in an edge statement:
+
+    [:start :> :middle :> :end]
+
+### Graph Attribute Statement
+
+A *graph attribute* statement sets graph-wide attributes. It is expressed as a map:
+
+    {:label "process #1", :style :filled, :color :lightgrey}
+    ; => graph [label="process #1",style=filled,color=lightgrey];
+
+alternatively, this can be expressed as a vector like this:
+
+    [:graph {:label "process #1", :style :filled, :color :lightgrey}]
+    ; => graph [label="process #1",style=filled,color=lightgrey];
+    
+### Node and Edge Attribute Statement
+A *node attribute* or *edge attribute* statement sets graph-wide node or edge attributes respectively. It is expressed as a vector:
+
+    [:node {:style :filled, :color :white}]
+    ; => node [style=filled,color=white];
+
+or:
+
+    [:edge {:color :black}]
+    ; => edge [color=black];
+
+## Defining Graphs
+As mentioned above, a graph consists of a series of statements. These statements are passed to the `graph`, `digraph`, or `subgraph` functions. Each takes an optional set of attributes followed by a vector of statements:
+
+    ; From http://www.graphviz.org/content/cluster
+    (digraph [
+      (subgraph :cluster_0 [
+        {:style :filled, :color :lightgrey, :label "process #1"}
+        [:node {:style :filled, :color :white}]
+
+        [:a0 :> :a1 :> :a2 :> :a3]])
+
+      (subgraph :cluster_1 [
+        {:style :filled, :color :blue, :label "process #2"}
+        [:node {:style :filled}]
+
+        [:b0 :> :b1 :> :b2 :> :b3]])
+
+      [:start :a0]
+      [:start :b0]
+      [:a1    :b3]
+      [:b2    :a3]
+      [:a3    :a0]
+      [:a3    :end]
+      [:b3    :end]
+
+      [:start {:shape :Mdiamond}]
+      [:end   {:shape :Msquare}]])
+
+Similarly for `(graph)` (undirected graph) and `(subgraph)`. A second form of these functions takes an initial option map, or a string or keyword id for the graph:
+
+    (graph :graph-id ...)
+    ; => graph "graph-id" { ... }
+    
+    (digraph { :id :G :strict? true } ...)
+    ; => strict graph G { ... }
+    
+## Rendering to Graphviz dot format
+
+Given a graph built with the functions described above, use the `(dot)` function to generate Graphviz DOT output.
+
+    (use 'dorothy.core)
+    (def g (graph [ ... ]))
+    (dot g)
 
 ## License
 
--- src/dorothy/core.clj
+++ src/dorothy/core.clj
@@ -16,10 +16,11 @@
                     (str \" (cs/replace (str id) "\"" "\\\"") \"))
     :else         (escape-id (str id))))
 
+(declare to-dottable)
 (defprotocol Dot 
   (dot* [this]))
 
-(defn statements [& ss]
+(defn statements [ss]
   (reify Dot
     (dot* [this]
       (apply str (map #(str (dot* %) ";\n") ss)))))
@@ -47,7 +48,7 @@
       (cs/join \, (for [[k v] options] (dot* (attr k v)))))))
 
 (defn- trailing-attrs [attr-map]
-  (if attr-map (str " [" (dot* (attrs attr-map)) "]")))
+  (if-not (empty? attr-map) (str " [" (dot* (attrs attr-map)) "]")))
 
 (defn- x-attrs [type options]
   (reify Dot
@@ -64,9 +65,9 @@
       (dot* [this]
         (str (dot* id) (trailing-attrs attr-map)))))
   ([id]
-    (node nil id)))
+    (node {} id)))
 
-(defn edge [attr-map & node-ids]
+(defn edge [attr-map node-ids]
   (reify Dot
     (dot* [this] 
       (str 
@@ -79,7 +80,7 @@
     :digraph  (assoc *options* :edge-op "->")
     :subgraph *options*))
 
-(defn graph [opts & stmts]
+(defn graph* [opts stmts]
   (let [{:keys [type id strict?]} opts 
         type (or type :graph)]
     (reify Dot
@@ -88,42 +89,117 @@
           (str (if strict? "strict ") 
                (name type) " "
                (if id (str (escape-id id) " ")) 
-               "{\n" (dot* (apply statements stmts)) "} "))))))
+               "{\n" (dot* (statements stmts)) "} "))))))
 
-(defn digraph [opts & stmts]
-  (apply graph (assoc opts :type :digraph) stmts))
+(defn digraph* [opts stmts]
+  (graph* (assoc opts :type :digraph) stmts))
 
-(defn subgraph [opts & stmts]
-  (apply graph (assoc opts :type :subgraph) stmts))
+(defn subgraph* [opts stmts]
+  (graph* (assoc opts :type :subgraph) stmts))
+
+(defn- vector-to-dottable-edge [v]
+  (let [end (last v)
+        attrs? (map? end)
+        attrs (if attrs? end {})
+        parts (if attrs? (butlast v) v)
+        parts (remove #{:>} parts)]
+    (edge attrs (map to-dottable parts))))
+
+(defn- vector-to-dottable [[v0 v1 & more :as v]]
+  (cond
+    (= v0 :graph) (graph-attrs v1)
+    (= v0 :node)  (node-attrs  v1)
+    (= v0 :edge)  (edge-attrs  v1)
+    more          (vector-to-dottable-edge v)
+    (map? v1)     (node v1 (to-dottable v0))
+    v1            (vector-to-dottable-edge v)
+    (map? v0)     (graph-attrs v0)
+    v0            (node (to-dottable v0))))
+
+(defn to-dottable [v]
+  (cond
+    (satisfies? Dot v) v
+    (keyword? v)       (node-id v)
+    (string?  v)       (node-id v)
+    (map? v)           (graph-attrs v)
+    (vector? v)        (vector-to-dottable v)))
+
+(defn- canonicalize-graph-attrs [attrs]
+  (cond
+    (map? attrs) attrs
+    (keyword? attrs) {:id attrs}
+    (number? attrs)  {:id (str attrs)}
+    (string? attrs)  {:id attrs}
+    :else            (throw (IllegalArgumentException. (str "Invalid graph arg " attrs)))))
+
+(defn graph 
+  ([attrs stmts]
+   (graph* (canonicalize-graph-attrs attrs) (map to-dottable stmts)))
+  ([stmts] (graph {} stmts)))
+(defn digraph 
+  ([attrs stmts]
+   (digraph* (canonicalize-graph-attrs attrs) (map to-dottable stmts)))
+  ([stmts] (digraph {} stmts)))
+(defn subgraph 
+  ([attrs stmts]
+   (subgraph* (canonicalize-graph-attrs attrs) (map to-dottable stmts)))
+  ([stmts] (subgraph {} stmts)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (println (dot* (attrs {:style :filled :color :blue :text "foo\"bar"})))
 (println (dot* (node-id "start" "p" :ne)))
 (println (dot* (node-id "start" "p")))
-(println (dot* (statements (node-id :start)(node-id "start" "p"))))
+(println (dot* (statements [(node-id :start)(node-id "start" "p")])))
 (println (dot* (node (node-id :start))))
 (println (dot* (node {:style :filled :color :blue} (node-id :start) )))
-(println (dot* (edge nil (node-id :start)(node-id :end))))
+(println (dot* (edge {} [(node-id :start)(node-id :end)])))
 (println (binding [*options* {:edge-op "--"}] 
-  (dot* (edge {:color :grey} (node-id :start)(node-id :middle :p :_)(node-id :end)))))
+  (dot* (edge {:color :grey} [(node-id :start)(node-id :middle :p :_)(node-id :end)]))))
 (println (dot* (graph-attrs {:style :filled})))
 (println (dot* (node-attrs {:style :filled, :color :red})))
 (println (dot* (edge-attrs {:style :filled})))
 (println (dot* (attr :color :lightgrey)))
 
 (println (dot*
-  (graph
-    {:id :G :strict? true}
-    (edge nil (node-id "start") (node-id :a0))
-    (edge {:color :green} (node-id :a0) 
-          (subgraph nil (edge nil (node-id :a) (node-id :b)))
-          (node-id :a1))
-    (node {:shape :Mdiamond} (node-id :start)))))
+  (graph*
+     {:id :G :strict? true}
+    [(edge nil [(node-id "start") (node-id :a0)])
+     (edge {:color :green} 
+          [(node-id :a0) 
+           (subgraph* {} [(edge {} [(node-id :a) (node-id :b)])])
+           (node-id :a1)])
+     (node {:shape :Mdiamond} (node-id :start))])))
 
 (println (dot*
-  (digraph
+  (digraph*
     ;{:id :G :strict? true}
-    nil
-    (edge nil (node-id "start") (node-id :a0))
-    (edge {:color :gre_en :text "hello\"there"} (node-id :a0) (node-id :a1))
-    (node {:shape :Mdiamond} (node-id :start)))))
+    {} 
+    [(edge nil [(node-id "start") (node-id :a0)])
+     (edge {:color :gre_en :text "hello\"there"} [(node-id :a0) (node-id :a1)])
+     (node {:shape :Mdiamond} (node-id :start))])))
 
+(println (dot* (digraph :G [
+  (subgraph :cluster_0 [
+    {:style :filled, :color :lightgrey, :label "process #1"}
+    [:node {:style :filled, :color :white}]
+
+    [:a0 :> :a1 :> :a2 :> :a3]])
+
+  (subgraph :cluster_1 [
+    {:style :filled, :color :blue, :label "process #2"}
+    [:node {:style :filled}]
+
+    [:b0 :> :b1 :> :b2 :> :b3]])
+
+  [:start :a0]
+  [:start :b0]
+  [:a1    :b3]
+  [:b2    :a3]
+  [:a3    :a0]
+  [:a3    :end]
+  [:b3    :end]
+
+  [:start {:shape :Mdiamond}]
+  [:end   {:shape :Msquare}]])))
