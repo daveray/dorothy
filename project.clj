17b9d5701aeb7f9e53908ac8559fbc474dabf2de
Dave Ray <daveray@gmail.com>
2011-08-12 22:00:21 -0400

    update project.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,9 @@
 (defproject dorothy "0.0.1-SNAPSHOT"
   :description "Hiccup-style generation of Graphviz graphs"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :url "https://github.com/daveray/dorothy"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dev-dependencies [[lein-clojars "0.6.0"]])
