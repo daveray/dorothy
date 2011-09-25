bedf23a12d01f4839e19c1ea635e06cac437240a
Dave Ray <daveray@gmail.com>
2011-09-24 20:59:50 -0400

    0.0.4 dev

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dorothy "0.0.3"
+(defproject dorothy "0.0.4-SNAPSHOT"
   :description "Hiccup-style generation of Graphviz graphs"
   :url "https://github.com/daveray/dorothy"
   :license {:name "Eclipse Public License - v 1.0"
